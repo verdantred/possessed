2cddfc98b983d38cd5b990f2120d732af7a64730
Ville Niittunen <ww.niittunen@microsoft.com>
2016-08-05 13:18:30 +0300

    Implement views and changing between them

--- src/main.rs
+++ src/main.rs
@@ -3,41 +3,7 @@ extern crate sdl2;
 mod phi;
 mod views;
 
-use sdl2::pixels::Color;
-use phi::Events;
-
 
 fn main() {
-    // Initialize SDL2
-    let sdl_context = sdl2::init().unwrap();
-    let video = sdl_context.video().unwrap();
-
-    // Create the window
-    let window = video.window("Possessed Game", 800, 600)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
-
-    let mut renderer = window.renderer()
-        .accelerated()
-        .build()
-        .unwrap();
-
-    // Prepare the events record
-    let mut events = Events::new(sdl_context.event_pump().unwrap());
-
-    loop {
-        events.pump();
-
-        if events.now.quit || events.now.key_escape == Some(true) {
-            break;
-        }
-
-        // Render a fully black window
-        renderer.set_draw_color(Color::RGB(0, 0, 0));
-        renderer.clear();
-        renderer.present();
-    }
-
+    phi::spawn("ArcadeRS Shooter", |_| Box::new(views::DefaultView));
 }
--- src/phi/mod.rs
+++ src/phi/mod.rs
@@ -9,7 +9,8 @@ struct_events! {
     keyboard: {
         key_escape: Escape,
         key_up: Up,
-        key_down: Down
+        key_down: Down,
+        key_space: Space
     },
     other: {
         quit: Quit { .. }
@@ -30,6 +31,7 @@ pub struct Phi<'window> {
 pub enum ViewAction {
     None,
     Quit,
+    ChangeView(Box<View>),
 }
 
 
@@ -39,4 +41,102 @@ pub trait View {
     ///
     /// `elapsed` is expressed in seconds.
     fn render(&mut self, context: &mut Phi, elapsed: f64) -> ViewAction;
+}
+
+/// Create a window with name `title`, initialize the underlying libraries and
+/// start the game with the `View` returned by `init()`.
+///
+/// # Examples
+///
+/// Here, we simply show a window with color #ffff00 and exit when escape is
+/// pressed or when the window is closed.
+///
+/// ```
+/// struct MyView;
+///
+/// impl View for MyView {
+///     fn render(&mut self, context: &mut Phi, _: f64) -> ViewAction {
+///         if context.events.now.quit {
+///             return ViewAction::Quit;
+///         }
+///
+///         context.renderer.set_draw_color(Color::RGB(255, 255, 0));
+///         context.renderer.clear();
+///         ViewAction::None
+///     }
+/// }
+///
+/// spawn("Example", |_| {
+///     Box::new(MyView)
+/// });
+/// ```
+pub fn spawn<F>(title: &str, init: F)
+    where F: Fn(&mut Phi) -> Box<View>
+{
+    // Initialize SDL2
+    let sdl_context = ::sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+    let mut timer = sdl_context.timer().unwrap();
+
+    // Create the window
+    let window = video.window(title, 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    // Create the context
+    let mut context = Phi {
+        events: Events::new(sdl_context.event_pump().unwrap()),
+        renderer: window.renderer()
+            .accelerated()
+            .build()
+            .unwrap(),
+    };
+
+    // Create the default view
+    let mut current_view = init(&mut context);
+
+
+    // Frame timing
+
+    let interval = 1_000 / 72;
+    let mut before = timer.ticks();
+    let mut last_second = timer.ticks();
+    let mut fps = 0u16;
+
+    loop {
+        // Frame timing (bis)
+
+        let now = timer.ticks();
+        let dt = now - before;
+        let elapsed = dt as f64 / 1_000.0;
+
+        // If the time elapsed since the last frame is too small, wait out the
+        // difference and try again.
+        if dt < interval {
+            timer.delay(interval - dt);
+            continue;
+        }
+
+        before = now;
+        fps += 1;
+
+        if now - last_second > 1_000 {
+            println!("FPS: {}", fps);
+            last_second = now;
+            fps = 0;
+        }
+
+
+        // Logic & rendering
+
+        context.events.pump();
+
+        match current_view.render(&mut context, elapsed) {
+            ViewAction::None => context.renderer.present(),
+            ViewAction::Quit => break,
+            ViewAction::ChangeView(new_view) => current_view = new_view,
+        }
+    }
 }
